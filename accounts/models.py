b7d242bf08108ddeeeb53e1c81bef75c37ff7a2a
IsraelBR <israeelbr14@gmail.com>
2016-09-10 12:23:03 -0500

    Modificacion de la base de datos

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,9 @@ class Medico(models.Model):
     	 ('hombre','Hombre'),
     	 ('mujer','Mujer'),
     ]
+	nombre = models.CharField(max_length = 50, null=True, blank=True)
+	apellido_materno = models.CharField(max_length = 50, null=True, blank=True)
+	apellido_paterno = models.CharField(max_length = 50, null=True, blank=True)
 	edad = models.IntegerField()
 	sexo = models.CharField(max_length=140, choices=OPCIONES,default="Hombre")
 	ESPECIALIAD = [
@@ -20,10 +23,11 @@ class Medico(models.Model):
 		('materiales-detales','Materiales Dentales'),
 		('patoliga-bucal','Patologia Bucal'),	
     ]
+	email = models.CharField(max_length = 50, null=True, blank=True)
 	especialidad = models.CharField(max_length=140,choices=ESPECIALIAD,default="Ortodoncia")
 	telefono = models.IntegerField() 
 	foto = models.ImageField(upload_to='fotomedico')
 	user = models.OneToOneField(User)
 
 	def __str__(self):
-		return '{} {}'.format(self.nombre,self.apellidos)
+		return '{} {}'.format(self.nombre, self.apellido_paterno)
